7155cdab1ac11d550ab31132cfa2c66fb49fa112
espi <jordiespinosa@gmail.com>
2026-06-20 11:19:36 +0200

    add virtual joystick

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781470124|1260357651';
+const CACHE_VERSION = '1781947125|501129801';
 /** @type {string} */
 const CACHE_PREFIX = 'Eko Guardians de-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
